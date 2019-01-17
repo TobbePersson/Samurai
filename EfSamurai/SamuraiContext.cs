1378bae75381a02b3bc9b34ad983fa5c33b66907
TobbePersson <spara.energi@gmail.com>
2019-01-17 14:03:21 +0100

    no message

---
+++ EfSamurai/SamuraiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EfSamurai.Domain;
+
+namespace EfSamurai.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = EfSamurai;");
+        }
+
+    }
+}
